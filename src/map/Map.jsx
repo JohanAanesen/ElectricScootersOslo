ca6636a25ecb30e719550c91362be6551c7fd767
Johan Aanesen <johan.aanesen@inmeta.no>
2019-09-05 17:00:40 +0200

    fetch from tier now works

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Tier from "../tier/Tier";
 
 export default function Map() {
-	return <h1>Map works!</h1>;
+	return (
+		<>
+			<h1>Map works!</h1>
+			<Tier></Tier>
+		</>
+	);
 }
--- src/tier/Tier.jsx
+++ src/tier/Tier.jsx
@@ -1,1 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import http from "../http.service";
+import { ApiURL } from "../api-config";
+
+export default function Tier() {
+	const options = {
+		headers: { "x-api-key": ApiURL.tierApiKey }
+	};
+
+	const [bikes, setBikes] = useState([]);
+
+	useEffect(() => {
+		const tempBikes = onLoad();
+		setBikes(tempBikes);
+	}, []);
+
+	const onLoad = async () => {
+		return await getTier().data;
+	};
+
+	async function getTier() {
+		console.log("ey");
+		return await http
+			.get(ApiURL.tier, {
+				headers: {
+                    "x-api-key": ApiURL.tierApiKey,
+                    'Content-Type': 'application/json',
+				},
+				
+			})
+			.then(res => {
+				console.log(res.data);
+				setBikes(res.data);
+			});
+
+		console.log("ney");
+	}
+
+	return (
+		<>
+			<h1>Tier works!</h1>
+		</>
+	);
+}
