0f24911e73cdd6983ed9fd322db0095ccfcd6d61
Johan Aanesen <johan.aanesen@inmeta.no>
2019-09-05 14:18:09 +0200

    added axios service and api keys

---
+++ src/api-config.js
@@ -0,0 +1,8 @@
+export const ApiURL = {
+	circ:
+		"https://api.goflash.com/api/Mobile/Scooters?userLatitude=59.92&userLongitude=10.75&lang=en&latitude=59.925733&longitude=10.757766&latitudeDelta=0.01&longitudeDelta=0.01",
+	voi:
+		"https://api.voiapp.io/v1/vehicle/status/ready?lat=59.925733&lng=10.757766",
+	tier: "https://platform.tier-services.io/vehicle?zoneId=OSLO",
+	tierApiKey: "bpEUTJEBTf74oGRWxaIcW7aeZMzDDODe1yBoSxi2"
+};
