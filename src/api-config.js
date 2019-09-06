7921d4e4702c8f6e21040932cdea81e2299f57be
Johan Aanesen <johan.aanesen@inmeta.no>
2019-09-06 13:23:44 +0200

    shoot me

--- src/api-config.js
+++ src/api-config.js
@@ -1,6 +1,6 @@
 export const ApiURL = {
 	circ:
-		"https://cors-anywhere.herokuapp.com/https://api.goflash.com/api/Mobile/Scooters?userLatitude=59.92&userLongitude=10.75&lang=en&latitude=59.925733&longitude=10.757766&latitudeDelta=0.01&longitudeDelta=0.01",
+		"https://api.goflash.com/api/Mobile/Scooters?userLatitude=59.92&userLongitude=10.75&lang=en&latitude=59.925733&longitude=10.757766&latitudeDelta=1&longitudeDelta=1",
 	voi:
 		"https://cors-anywhere.herokuapp.com/https://api.voiapp.io/v1/vehicle/status/ready?lat=59.925733&lng=10.757766",
 	tier: "https://cors-anywhere.herokuapp.com/https://platform.tier-services.io/vehicle?zoneId=OSLO",
